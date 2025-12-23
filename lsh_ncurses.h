69436c16d72b8c25cb0e55e21c9b263d70e515a9
lukas <lukas.hartwig@gmx.net>
2025-12-23 17:57:41 +0100

    Restore lsh_ncurses.h

--- lsh_ncurses.h
+++ lsh_ncurses.h
@@ -1,67 +1,45 @@
-#pragma once
+#ifndef lsh_ncurses_h
+#define lsh_ncurses_h
+
+#include "lsh_types.h"
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+#define CTRL(c) ((c) & 037)
+#define LK_NEWLINE 10
+#define LK_ENTER 13
+#define LK_UP 65
+#define LK_DOWN 258
+#define LK_RIGHT 67
+#define LK_LEFT 68
+#define LK_ESCAPE 27
+#define LK_SPACE 32
 
 void ncurses_init();
 
 #ifdef LSH
 
-char *get_path() {
-  #define PATH_MAX 4096
-  char buffer[PATH_MAX];
-  if (getcwd(buffer, PATH_MAX) == NULL) {
-    perror("getcwd");
-    return NULL;
-  }
-  u16 cwd_len = strlen(buffer) + 1;
-  char *path = malloc(cwd_len);
-  strcpy(path, buffer);
-  return path;
-}
-
-bool is_directory(char *path) {
-  struct stat statbuf;
-  if (stat(path, &statbuf) != 0)
-    die("stat dieded: %s", path);
-  return S_ISDIR(statbuf.st_mode);
-}
-
-typedef enum {
-  BLINKING_BLOCK = 1,
-  STEADY_BLOCK,
-  BLINKING_UNDERLINE,
-  STEADY_UNDERLINE,
-  BLINKING_BAR,
-  STEADY_BAR,
-} CursorShape;
-
-void set_cursor_shape(int code) {
-  printf("\033[%d q", code);
-  fflush(stdout); // Ensure it gets sent to terminal
+void die(const char *format, ...) {
+  endwin();
+  va_list args;
+  va_start(args, format);
+    vprintf(format, args);
+  va_end(args);
+  printf("\n");
+  exit(0);
 }
 
 void ncurses_init() {
   initscr();
   noecho();
-  curs_set(1);
-  set_cursor_shape(BLINKING_BAR);
   start_color();
   atexit((void*)endwin);
   if (!has_colors()) {
-    die("No Colors\n");
-  }
-  if (can_change_color()) {
-    // change colors
+    exit(1);
   }
   raw();
-  nonl(); // for LK_ENTER|13
-  setup_signals();
-  
-  for (u8 i = 0; i < 8; i++) {
-    for (u8 j = 0; j < 8; j++) {
-      init_pair(i * 8 + j, j, i);
-    }
-  }
-
-  // INVERTED AUTOMATICALLY (B, B) -> (W, B) 
+  nonl();
   // 0 = BLACK
   // 1 = RED
   // 2 = GREEN
@@ -70,6 +48,12 @@ void ncurses_init() {
   // 5 = PURPLE
   // 6 = TEAL
   // 7 = WHITE
+  for (u8 i = 0; i < 8; i++) {
+    for (u8 j = 0; j < 8; j++) {
+      init_pair(i * 8 + j, j, i);
+    }
+  }
 }
 
+#endif
 #endif
